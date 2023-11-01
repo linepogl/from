e4e39c74f3aa5ba0bd0a85ed447c8df7fa1e9fb7
Lazare Inepologlou <linepogl@gmail.com>
2023-11-01 16:59:24 +0100

    code style

--- tst/FromTest.php
+++ tst/FromTest.php
@@ -23,22 +23,22 @@ final class FromTest extends TestCase
 
     public function test_map(): void
     {
-        $this->assertSame([], from([])->map(static fn ($x) => $x + 1)->toArray());
-        $this->assertSame([2, 3, 4], from([1, 2, 3])->map(static fn ($x) => $x + 1)->toArray());
-        $this->assertSame([1, 3, 5], from([1, 2, 3])->map(static fn ($x, $k) => $x + $k)->toArray());
-        $this->assertSame(['a' => 2, 3, 4], from(['a' => 1, 2, 3])->map(static fn ($x) => $x + 1)->toArray());
-        $this->assertSame(['a' => '1a', '20', '31'], from(['a' => 1, 2, 3])->map(static fn ($x, $k) => $x . $k)->toArray());
+        $this->assertSame([], from([])->map(fn ($x) => $x + 1)->toArray());
+        $this->assertSame([2, 3, 4], from([1, 2, 3])->map(fn ($x) => $x + 1)->toArray());
+        $this->assertSame([1, 3, 5], from([1, 2, 3])->map(fn ($x, $k) => $x + $k)->toArray());
+        $this->assertSame(['a' => 2, 3, 4], from(['a' => 1, 2, 3])->map(fn ($x) => $x + 1)->toArray());
+        $this->assertSame(['a' => '1a', '20', '31'], from(['a' => 1, 2, 3])->map(fn ($x, $k) => $x . $k)->toArray());
     }
 
     public function test_flat_map(): void
     {
-        $this->assertSame([], from([])->flatMap(static fn ($x, $y): array => [$x + $y])->toArray());
-        $this->assertSame([2, 3, 4], array_values(from([1, 2, 3])->flatMap(static fn ($x, $y): array => [$x + 1])->toArray()));
+        $this->assertSame([], from([])->flatMap(fn ($x, $y): array => [$x + $y])->toArray());
+        $this->assertSame([2, 3, 4], array_values(from([1, 2, 3])->flatMap(fn ($x, $y): array => [$x + 1])->toArray()));
     }
 
     public function test_map_keys(): void
     {
-        $this->assertSame(['a' => 'a', 'b' => 'b'], from([0 => 'a', 1 => 'b'])->mapKeys(static fn ($x, $y) => $x)->toArray());
+        $this->assertSame(['a' => 'a', 'b' => 'b'], from([0 => 'a', 1 => 'b'])->mapKeys(fn ($x, $y) => $x)->toArray());
     }
 
     public function test_merge(): void
@@ -50,11 +50,11 @@ final class FromTest extends TestCase
 
     public function test_map_with_keys(): void
     {
-        $this->assertSame([], from([])->map(static fn ($x) => $x + 1, static fn ($x, $k) => $x . $k)->toArray());
-        $this->assertSame(['10' => 2, '21' => 3, '32' => 4], from([1, 2, 3])->map(static fn ($x) => $x + 1, static fn ($x, $k) => $x . $k)->toArray());
-        $this->assertSame(['10' => 1, '21' => 3, '32' => 5], from([1, 2, 3])->map(static fn ($x, $k) => $x + $k, static fn ($x, $k) => $x . $k)->toArray());
-        $this->assertSame(['1a' => 2, '20' => 3, '31' => 4], from(['a' => 1, 2, 3])->map(static fn ($x) => $x + 1, static fn ($x, $k) => $x . $k)->toArray());
-        $this->assertSame(['1a' => '1a', '20' => '20', '31' => '31'], from(['a' => 1, 2, 3])->map(static fn ($x, $k) => $x . $k, static fn ($x, $k) => $x . $k)->toArray());
+        $this->assertSame([], from([])->map(fn ($x) => $x + 1, fn ($x, $k) => $x . $k)->toArray());
+        $this->assertSame(['10' => 2, '21' => 3, '32' => 4], from([1, 2, 3])->map(fn ($x) => $x + 1, fn ($x, $k) => $x . $k)->toArray());
+        $this->assertSame(['10' => 1, '21' => 3, '32' => 5], from([1, 2, 3])->map(fn ($x, $k) => $x + $k, fn ($x, $k) => $x . $k)->toArray());
+        $this->assertSame(['1a' => 2, '20' => 3, '31' => 4], from(['a' => 1, 2, 3])->map(fn ($x) => $x + 1, fn ($x, $k) => $x . $k)->toArray());
+        $this->assertSame(['1a' => '1a', '20' => '20', '31' => '31'], from(['a' => 1, 2, 3])->map(fn ($x, $k) => $x . $k, fn ($x, $k) => $x . $k)->toArray());
     }
 
     public function test_take(): void
@@ -79,48 +79,48 @@ final class FromTest extends TestCase
 
     public function test_filter(): void
     {
-        $this->assertSame([], from([])->filter(static fn ($x) => $x > 1)->toArray());
-        $this->assertSame([1 => 2, 2 => 3], from([1, 2, 3])->filter(static fn ($x) => $x > 1)->toArray());
-        $this->assertSame([2 => 3], from([1, 2, 3])->filter(static fn ($x, $k) => $k > 1)->toArray());
-        $this->assertSame([1], from([1, 2, 3])->filter(static fn ($x, $k) => $k < 1)->toArray());
-        $this->assertSame([2, 3], from(['a' => 1, 2, 3])->filter(static fn ($x) => $x > 1)->toArray());
-        $this->assertSame([2], from(['a' => 1, 2, 3])->filter(static fn ($x, $k) => $k === 0)->toArray());
+        $this->assertSame([], from([])->filter(fn ($x) => $x > 1)->toArray());
+        $this->assertSame([1 => 2, 2 => 3], from([1, 2, 3])->filter(fn ($x) => $x > 1)->toArray());
+        $this->assertSame([2 => 3], from([1, 2, 3])->filter(fn ($x, $k) => $k > 1)->toArray());
+        $this->assertSame([1], from([1, 2, 3])->filter(fn ($x, $k) => $k < 1)->toArray());
+        $this->assertSame([2, 3], from(['a' => 1, 2, 3])->filter(fn ($x) => $x > 1)->toArray());
+        $this->assertSame([2], from(['a' => 1, 2, 3])->filter(fn ($x, $k) => $k === 0)->toArray());
     }
 
     public function test_reject(): void
     {
-        $this->assertSame([], from([])->reject(static fn ($x) => $x > 1)->toArray());
-        $this->assertSame([1], from([1, 2, 3])->reject(static fn ($x) => $x > 1)->toArray());
-        $this->assertSame([1, 2], from([1, 2, 3])->reject(static fn ($x, $k) => $k > 1)->toArray());
-        $this->assertSame([1 => 2, 2 => 3], from([1, 2, 3])->reject(static fn ($x, $k) => $k < 1)->toArray());
-        $this->assertSame(['a' => 1], from(['a' => 1, 2, 3])->reject(static fn ($x) => $x > 1)->toArray());
-        $this->assertSame(['a' => 1, 1 => 3], from(['a' => 1, 2, 3])->reject(static fn ($x, $k) => $k === 0)->toArray());
+        $this->assertSame([], from([])->reject(fn ($x) => $x > 1)->toArray());
+        $this->assertSame([1], from([1, 2, 3])->reject(fn ($x) => $x > 1)->toArray());
+        $this->assertSame([1, 2], from([1, 2, 3])->reject(fn ($x, $k) => $k > 1)->toArray());
+        $this->assertSame([1 => 2, 2 => 3], from([1, 2, 3])->reject(fn ($x, $k) => $k < 1)->toArray());
+        $this->assertSame(['a' => 1], from(['a' => 1, 2, 3])->reject(fn ($x) => $x > 1)->toArray());
+        $this->assertSame(['a' => 1, 1 => 3], from(['a' => 1, 2, 3])->reject(fn ($x, $k) => $k === 0)->toArray());
     }
 
     public function test_any(): void
     {
-        $this->assertFalse(from([])->any(static fn ($x) => $x > 1));
-        $this->assertTrue(from([1, 2, 3])->any(static fn ($x) => $x > 1));
-        $this->assertFalse(from([1, 2, 3])->any(static fn ($x) => $x < 0));
-        $this->assertTrue(from([1, 2, 3])->any(static fn ($x, $k) => $k > 1));
-        $this->assertFalse(from([1, 2, 3])->any(static fn ($x, $k) => $k < 0));
-        $this->assertTrue(from(['a' => 1, 2, 3])->any(static fn ($x) => $x === 2));
-        $this->assertFalse(from(['a' => 1, 2, 3])->any(static fn ($x) => $x === 4));
-        $this->assertTrue(from(['a' => 1, 2, 3])->any(static fn ($x, $k) => $k === 'a'));
-        $this->assertFalse(from(['a' => 1, 2, 3])->any(static fn ($x, $k) => $k === 'b'));
+        $this->assertFalse(from([])->any(fn ($x) => $x > 1));
+        $this->assertTrue(from([1, 2, 3])->any(fn ($x) => $x > 1));
+        $this->assertFalse(from([1, 2, 3])->any(fn ($x) => $x < 0));
+        $this->assertTrue(from([1, 2, 3])->any(fn ($x, $k) => $k > 1));
+        $this->assertFalse(from([1, 2, 3])->any(fn ($x, $k) => $k < 0));
+        $this->assertTrue(from(['a' => 1, 2, 3])->any(fn ($x) => $x === 2));
+        $this->assertFalse(from(['a' => 1, 2, 3])->any(fn ($x) => $x === 4));
+        $this->assertTrue(from(['a' => 1, 2, 3])->any(fn ($x, $k) => $k === 'a'));
+        $this->assertFalse(from(['a' => 1, 2, 3])->any(fn ($x, $k) => $k === 'b'));
     }
 
     public function test_all(): void
     {
-        $this->assertTrue(from([])->all(static fn ($x) => $x > 1));
-        $this->assertTrue(from([1, 2, 3])->all(static fn ($x) => $x > 0));
-        $this->assertFalse(from([1, 2, 3])->all(static fn ($x) => $x > 1));
-        $this->assertTrue(from([1, 2, 3])->all(static fn ($x, $k) => $k < 3));
-        $this->assertFalse(from([1, 2, 3])->all(static fn ($x, $k) => $k < 2));
-        $this->assertTrue(from(['a' => 1, 2, 3])->all(static fn ($x) => $x > 0));
-        $this->assertFalse(from(['a' => 1, 2, 3])->all(static fn ($x) => $x > 1));
-        $this->assertTrue(from(['a' => 1, 2, 3])->all(static fn ($x, $k) => is_int($k) || $k === 'a'));
-        $this->assertFalse(from(['a' => 1, 2, 3])->all(static fn ($x, $k) => $k === 'a'));
+        $this->assertTrue(from([])->all(fn ($x) => $x > 1));
+        $this->assertTrue(from([1, 2, 3])->all(fn ($x) => $x > 0));
+        $this->assertFalse(from([1, 2, 3])->all(fn ($x) => $x > 1));
+        $this->assertTrue(from([1, 2, 3])->all(fn ($x, $k) => $k < 3));
+        $this->assertFalse(from([1, 2, 3])->all(fn ($x, $k) => $k < 2));
+        $this->assertTrue(from(['a' => 1, 2, 3])->all(fn ($x) => $x > 0));
+        $this->assertFalse(from(['a' => 1, 2, 3])->all(fn ($x) => $x > 1));
+        $this->assertTrue(from(['a' => 1, 2, 3])->all(fn ($x, $k) => is_int($k) || $k === 'a'));
+        $this->assertFalse(from(['a' => 1, 2, 3])->all(fn ($x, $k) => $k === 'a'));
     }
 
     public function test_compact(): void
@@ -161,9 +161,9 @@ final class FromTest extends TestCase
 
     public function test_reduce(): void
     {
-        $this->assertSame(0, from([3, 2, 1, 0])->reduce(static fn ($x, $y) => $y));
-        $this->assertSame(1, from([3, 2, 1, 0])->reduce(static fn ($x, $y) => $x, 1));
-        $this->assertSame('c', from(['a', 'b', 'c'])->reduce(static fn ($x, $y) => $y));
+        $this->assertSame(0, from([3, 2, 1, 0])->reduce(fn ($x, $y) => $y));
+        $this->assertSame(1, from([3, 2, 1, 0])->reduce(fn ($x, $y) => $x, 1));
+        $this->assertSame('c', from(['a', 'b', 'c'])->reduce(fn ($x, $y) => $y));
     }
 
     public function test_implode(): void
@@ -188,52 +188,52 @@ final class FromTest extends TestCase
             'b' => ['a' => 2, 'b' => 30],
             'c' => ['a' => 2, 'b' => 1],
             'a' => ['a' => 9, 'b' => 10],
-        ], from($a)->orderBy(static fn ($x) => $x['a'])->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'])->toArray());
         $this->assertSame([
             'a' => ['a' => 9, 'b' => 10],
             'b' => ['a' => 2, 'b' => 30],
             'c' => ['a' => 2, 'b' => 1],
-        ], from($a)->orderBy(static fn ($x) => $x['a'], desc: true)->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'], desc: true)->toArray());
         $this->assertSame([
             'c' => ['a' => 2, 'b' => 1],
             'b' => ['a' => 2, 'b' => 30],
             'a' => ['a' => 9, 'b' => 10],
-        ], from($a)->orderBy(static fn ($x) => $x['a'])->thenBy(static fn ($x) => $x['b'])->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'])->thenBy(fn ($x) => $x['b'])->toArray());
         $this->assertSame([
             'b' => ['a' => 2, 'b' => 30],
             'c' => ['a' => 2, 'b' => 1],
             'a' => ['a' => 9, 'b' => 10],
-        ], from($a)->orderBy(static fn ($x) => $x['a'])->thenBy(static fn ($x) => $x['b'], desc: true)->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'])->thenBy(fn ($x) => $x['b'], desc: true)->toArray());
         $this->assertSame([
             'a' => ['a' => 9, 'b' => 10],
             'c' => ['a' => 2, 'b' => 1],
             'b' => ['a' => 2, 'b' => 30],
-        ], from($a)->orderBy(static fn ($x) => $x['a'], desc: true)->thenBy(static fn ($x) => $x['b'])->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'], desc: true)->thenBy(fn ($x) => $x['b'])->toArray());
         $this->assertSame([
             'a' => ['a' => 9, 'b' => 10],
             'b' => ['a' => 2, 'b' => 30],
             'c' => ['a' => 2, 'b' => 1],
-        ], from($a)->orderBy(static fn ($x) => $x['a'], desc: true)->thenBy(static fn ($x) => $x['b'], desc: true)->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'], desc: true)->thenBy(fn ($x) => $x['b'], desc: true)->toArray());
 
         $this->assertSame([
             'a' => ['a' => 9, 'b' => 10],
             'b' => ['a' => 2, 'b' => 30],
             'c' => ['a' => 2, 'b' => 1],
-        ], from($a)->orderBy(static fn ($x, $k) => $k)->toArray());
+        ], from($a)->orderBy(fn ($x, $k) => $k)->toArray());
         $this->assertSame([
             'c' => ['a' => 2, 'b' => 1],
             'b' => ['a' => 2, 'b' => 30],
             'a' => ['a' => 9, 'b' => 10],
-        ], from($a)->orderBy(static fn ($x, $k) => $k, desc: true)->toArray());
+        ], from($a)->orderBy(fn ($x, $k) => $k, desc: true)->toArray());
         $this->assertSame([
             'b' => ['a' => 2, 'b' => 30],
             'c' => ['a' => 2, 'b' => 1],
             'a' => ['a' => 9, 'b' => 10],
-        ], from($a)->orderBy(static fn ($x) => $x['a'])->thenBy(static fn ($x, $k) => $k)->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'])->thenBy(fn ($x, $k) => $k)->toArray());
         $this->assertSame([
             'c' => ['a' => 2, 'b' => 1],
             'b' => ['a' => 2, 'b' => 30],
             'a' => ['a' => 9, 'b' => 10],
-        ], from($a)->orderBy(static fn ($x) => $x['a'])->thenBy(static fn ($x, $k) => $k, desc: true)->toArray());
+        ], from($a)->orderBy(fn ($x) => $x['a'])->thenBy(fn ($x, $k) => $k, desc: true)->toArray());
     }
 }
